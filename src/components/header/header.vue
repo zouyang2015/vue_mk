8c11a32f7792a762455e9fff4341ccec73a56097
zouyang@baokeyun.com <86022310@qq.com>
2017-02-23 23:39:34 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,9 +13,13 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletin-wrapper"></div>
@@ -28,6 +32,9 @@
       seller: {
         type: Object
       }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
@@ -37,12 +44,15 @@
 
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
         display: inline-block
+        img
+          border-radius: 2px
       .content
         display: inline-block
         margin-left: 16px
@@ -60,7 +70,54 @@
           .name
             margin-left: 6px
             font-size: 16px
-            color: rgb(255,255,255)
+            color: rgb(255, 255, 255)
             font-weight: bold
-            line-height: 16px
+            line-height: 18px
+        .description
+          margin-bottom: 10px
+          font-size: 12px
+          line-height: 12px
+        .support
+          font-size: 0
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-img('decrease_1')
+            &.discount
+              bg-img('discount_1')
+            &.special
+              bg-img('special_1')
+            &.invoice
+              bg-img('invoice_1')
+            &.guarantee
+              bg-img('guarantee_1')
+          .text
+            display: inline-block;
+            line-height: 12px
+            font-size: 10px
+
+
+      .support-count
+        position: absolute
+        padding: 0 8px
+        right: 12px
+        bottom: 14px
+        height: 24px
+        line-height: 24px
+        text-align: center
+        border-radius: 14px
+        background-color rgb(0,0,0,0.2)
+        .count
+          vertical-align: top
+          font-size: 10px
+        i
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
 </style>
