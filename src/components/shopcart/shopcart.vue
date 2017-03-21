9865e276bb5764b458bd34598c718facc9cf4edb
zouyang@baokeyun.com <86022310@qq.com>
2017-03-21 21:57:38 +0800

    shop cart 02

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -4,10 +4,10 @@
       <div class="shopcart-left">
         <div class="logo-wrapper">
           <div class="logo">
-            <span class="icon-shopping-cart"></span>
+            <i class="icon-shopping_cart"></i>
           </div>
         </div>
-        <div class="price"></div>
+        <div class="price">¥</div>
         <div class="desc"></div>
       </div>
       <div class="shopcart-right"></div>
@@ -16,7 +16,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-    export default {};
+  export default {};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -27,5 +27,51 @@
     z-index: 50
     width: 100%
     height: 48px
-    background: #000
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      .shopcart-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            text-align: center
+            background: #2b343c
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          padding-right: 12px
+          line-height: 24px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255,255,255,0.1)
+          font-size: 16px
+          font-weight: 700
+          color: rgba(255,255,255,0.4)
+        .desc
+          display: inline-block
+          vertical-align: top
+          line-height: 24px
+          margin-left: 12px
+      .shopcart-right
+        flex: 0 0 105px
+        width: 105px
 </style>
